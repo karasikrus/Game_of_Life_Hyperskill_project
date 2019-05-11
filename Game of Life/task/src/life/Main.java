9ca5eec1245e06c54f0fd0ae8fcc536788aa6883
karasikrus <karasikrus@gmail.com>
2019-05-11 17:08:17 +0300

    clear screen added

--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -1,11 +1,14 @@
 package life;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         StateGenerator stateGenerator = new StateGenerator();
+
+
         int n_lines = scanner.nextInt();
         long s_seed = scanner.nextLong();
         int m_number = scanner.nextInt();
@@ -13,8 +16,19 @@ public class Main {
         //field.print();
         //System.out.println("-------------next generation----------");
         for(int i =0; i < m_number; i++){
+            try{
+                if (System.getProperty("os.name").contains("Windows")){
+                    new ProcessBuilder("cmd","/c","cls").inheritIO().start();
+                } else{
+                    Runtime.getRuntime().exec("clear");
+                }
+            } catch (IOException e){}
+            System.out.println("Generation #"+ (i+1));
             field = stateGenerator.generateNextState(field);
+            field.print();
+            scanner.next();
         }
-        field.print();
+
+
     }
 }
